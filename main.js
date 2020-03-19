1608bb6493375abb144b87753fcaeaf47ec88b0a
sureshbabu345 <usureshbabu7777@gmail.com>
2020-03-19 10:46:31 +0000

    Lab 1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'yourDevServiceNowInstanceCanonicalHostname',
-  username: 'devUsername',
-  password: 'devUserPassword'
+  url: 'https://dev84054.service-now.com',
+  username: 'admin',
+  password: 'Suresh@123'
 };
 
 
